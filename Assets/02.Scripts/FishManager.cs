5dd3ac84ee8dedc3f2f680f4667ec55e54c8dc94
dlckdgh0414 <ci5976547@gmail.com>
2024-05-05 00:20:56 +0900

    Add:Fish Manager

--- Assets/02.Scripts/FishManager.cs
+++ Assets/02.Scripts/FishManager.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class FishManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float currentTime;
+    [SerializeField] private float creatTime = 2f;
+    public GameObject[] FishPrefab;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (creatTime < currentTime)
+        {
+            int FishIdex = Random.Range(0, FishPrefab.Length);
+            Instantiate(FishPrefab[FishIdex], transform.position, Quaternion.identity);
+            currentTime = 0;
+        }
+    }
+    private void FixedUpdate()
+    {
+        currentTime += Time.fixedDeltaTime;
     }
 }
