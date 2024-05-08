9d34169ef64507a0f042abd6bc0b5b956e542d0f
dlckdgh0414 <ci5976547@gmail.com>
2024-05-08 18:48:30 +0900

    Fix: CatMove

--- Assets/02.Scripts/CatFoot.cs
+++ Assets/02.Scripts/CatFoot.cs
@@ -8,22 +8,16 @@ public class CatFoot : MonoBehaviour
     [SerializeField] private GameObject catFoot;
     [SerializeField] private GameObject UI;
     [SerializeField] private GameObject UI1;
-    [SerializeField] private GameObject CatFootLeft;
-    int Count;
+    [SerializeField]int Count;
 
 
     public void OnClick()
     {
         StartCoroutine(catMove());
     }
-
-    public void OtherClick()
-    {
-        StartCoroutine(LeftMove());
-    }
     IEnumerator catMove()
     {
-        catFoot.transform.DOMove(new Vector3(-9.5f, 0, 0), 1f);
+        catFoot.transform.DOMove(new Vector3(-9f, 0, 0), 1f);
         yield return new WaitForSeconds(1.5f);
         catFoot.transform.DOMove(new Vector3(-20, 0, 0), 1f);
         if (Count == 0)
@@ -35,12 +29,5 @@ public class CatFoot : MonoBehaviour
         {
             UI1.transform.DOMove(new Vector3(-25, 0, 0), 1f);
         }
-    }
-    IEnumerator LeftMove()
-    {
-        CatFootLeft.transform.DOMove(new Vector3(-20, 0, 0), 2f);
-        yield return new WaitForSeconds(1.5f);
-        CatFootLeft.transform.DOMove(new Vector3(19, 0, 0), 2.8f);
-        UI.transform.DOMove(new Vector3(0, 0, 0), 2f);
     }
 }
--- Assets/02.Scripts/PlayerRotation.cs
+++ Assets/02.Scripts/PlayerRotation.cs
@@ -6,12 +6,47 @@ using DG.Tweening;
 public class PlayerDotween : MonoBehaviour
 {
     RaycastHit2D Hille;
-    private bool HilleHiet;
+    private bool HilleHit=true;
     [SerializeField] Transform drawLineObject;
-
+    [SerializeField] GameObject Player;
+    Rigidbody2D rigid;
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        Ray();
+    }
     private void OnDrawGizmos()
     {
         DrawRay();
+    }
+    private void Ray()
+    {
+        Hille = Physics2D.Raycast(rigid.position, Vector3.down, 3f, 1 << 3);
+        if(HilleHit)
+        {
+            if (Hille.collider.gameObject.tag == "Buttom")
+            {
+                HilleHit = false;
+                StartCoroutine(DelayHit(0.1f));
+                Player.transform.DORotate(new Vector3(0, 0, -30), 1f);
+                rigid.gravityScale = 1.5f;
+
+            }
+            else
+            {
+                HilleHit = false;
+                StartCoroutine(DelayHit(0.01f));
+                Player.transform.DORotate(new Vector3(0, 0, 0), 0.2f);
+            }
+        }
+    }
+    IEnumerator DelayHit(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        HilleHit = true;
     }
     private void DrawRay()
     {
